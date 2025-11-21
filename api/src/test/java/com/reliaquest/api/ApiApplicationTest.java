77b7c8ada1dc5ba470fbc66e9996914363c7a6c5
Ben Coombs <bjcoombs@users.noreply.github.com>
2025-11-21 11:55:19 +0000

    test: Improve test coverage for ApiApplication

--- api/src/test/java/com/reliaquest/api/ApiApplicationTest.java
+++ api/src/test/java/com/reliaquest/api/ApiApplicationTest.java
@@ -1,13 +1,26 @@
 package com.reliaquest.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class ApiApplicationTest {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
-    void someTest() {
-        // do the thing here
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
+
+    @Test
+    void mainMethodStartsApplication() {
+        // This covers the main method
+        ApiApplication.main(new String[] {});
     }
 }
